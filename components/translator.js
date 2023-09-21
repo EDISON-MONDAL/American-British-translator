720ca402ffdd0a32210c157e6dfcc398c6a168bc
Edison-Mondal <mondaledison@gmail.com>
2023-09-21 10:26:02 +0600

    ok

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,59 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
 
+class Translator {
+    ame2brits(input) {
+        const text = input
+
+
+        // Step 1: Define variables
+        const keyword = "apple";
+                
+
+        // Step 2: Build regex pattern as a string
+        const regexPattern = new RegExp(`[^a-zA-Z]${keyword}[^a-zA-Z]`, 'i');
+        const search1 = text.search(regexPattern);
+
+        const regexPattern2 = new RegExp(`${keyword}[^a-zA-Z]`, 'i');
+        let search2 = -1
+        if( text.search(regexPattern2) == 0){
+            search2 = text.search(regexPattern2);
+        }
+
+        const regexPattern3 = new RegExp(`[^a-zA-Z]${keyword}`, 'i');
+        let search3 = -1
+        if( text.search(regexPattern3) != -1){
+            const sliceBefore = text.slice(0, text.search(regexPattern3) )
+            const match = text.match( regexPattern3) 
+            // console.log('len '+ sliceBefore.length)
+            // console.log('len '+ match[0])
+            if( (sliceBefore.length +  match[0].length) == text.length ){
+                search3 = text.search(regexPattern3);
+            }
+        }
+
+
+        const regexPattern4 = new RegExp(`${keyword}`, 'i');
+        let search4 = -1
+        if( text.search(regexPattern4) == 0){
+            const sliceBefore = text.slice(0, text.search(regexPattern4) )
+            const match = text.match( regexPattern4)
+            
+            if( (sliceBefore.length +  match[0].length) == text.length ){
+                search4 = text.search(regexPattern4);
+            }
+        }
+
+
+
+        
+        console.log('1 '+ search1 ); // Output: [ 'apple' ]
+        console.log('2 '+ search2 );
+        console.log('3 '+ search3 );
+        console.log('4 '+ search4 );
+
+    };
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,31 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      let { text, locale } = req.body
       
+      if(text == undefined || locale == undefined){
+        return res.json({ error: 'Required field(s) missing' })
+      }
+
+      if(text == ''){
+        return res.json({ error: 'No text to translate' })
+      }
+
+      if(locale !== "american-to-british" && locale !== "british-to-american"  ){
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+
+      if(locale == "american-to-british" ){
+        const result = translator.ame2brits( text );
+
+        //res.json({ text, translation: result })
+
+        //console.warn(result)
+
+      } else if( locale == "british-to-american" ){
+
+      }
+
+
     });
 };
